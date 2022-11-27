17599ca59474d8c3772ce954293c24a8318e507d
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-11-27 19:29:59 +0900

    Add: キルされた時の挙動追加
    * キル時のログを変更
    * キルされた時に5秒間の観戦を追加

---
+++ spigot/src/main/java/com/nekozouneko/anni/listener/PlayerKillListener.java
@@ -0,0 +1,40 @@
+package com.nekozouneko.anni.listener;
+
+import com.nekozouneko.anni.ANNIPlugin;
+import com.nekozouneko.anni.task.SpectateKiller;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class PlayerKillListener implements Listener {
+
+    private final ANNIPlugin plugin = ANNIPlugin.getInstance();
+
+    @EventHandler
+    public void onEvent(PlayerDeathEvent e) {
+        Player killer = e.getEntity().getKiller();
+        Player killed = e.getEntity();
+
+        if (killer != null && killer != killed) {
+            World w = e.getEntity().getWorld();
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                if (p.getWorld() == w) {
+                    p.sendMessage("§7" + killed.getDisplayName() + " §f<- §7" + killer.getDisplayName());
+                }
+            }
+
+            e.setDeathMessage(null);
+            e.setDroppedExp(0);
+
+            killed.teleport(killer);
+            killed.setGameMode(GameMode.SPECTATOR);
+            new SpectateKiller(5, killed).runTaskTimer(plugin, 20, 20);
+        }
+
+    }
+
+}
