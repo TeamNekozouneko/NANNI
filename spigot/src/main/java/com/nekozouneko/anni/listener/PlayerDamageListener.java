ad27a548c9b721b739904118bd2dfe6dce448e91
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-11-28 16:06:06 +0900

    Add: ダメージを受けたときの透明化の解除機能追加

---
+++ spigot/src/main/java/com/nekozouneko/anni/listener/PlayerDamageListener.java
@@ -0,0 +1,26 @@
+package com.nekozouneko.anni.listener;
+
+import com.nekozouneko.nutilsxlib.chat.NChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.potion.PotionEffectType;
+
+public class PlayerDamageListener implements Listener {
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEvent(EntityDamageByEntityEvent e) {
+        if (e.getEntity() instanceof Player) {
+            Player p = (Player) e.getEntity();
+
+            if (p.hasPotionEffect(PotionEffectType.INVISIBILITY)) {
+                String d = e.getDamager().getCustomName();
+                d = d == null ? e.getDamager().getName() : d;
+                p.removePotionEffect(PotionEffectType.INVISIBILITY);
+                p.sendMessage(NChatColor.RED + d + "の攻撃により透明化が解除されました");
+
+            }
+        }
+    }
+
+}
