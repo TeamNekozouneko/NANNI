0b6f713845fe8c05776ca724866fa87660ba54f9
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-11-28 16:07:55 +0900

    Add: 一部のサブコマンドを実装

--- spigot/src/main/java/com/nekozouneko/anni/command/ANNIAdminCommand.java
+++ spigot/src/main/java/com/nekozouneko/anni/command/ANNIAdminCommand.java
@@ -1,22 +1,22 @@
 package com.nekozouneko.anni.command;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.nekozouneko.anni.ANNIPlugin;
+import com.nekozouneko.anni.file.ANNILobby;
 import com.nekozouneko.anni.file.ANNIMap;
-import com.nekozouneko.nutilsxlib.chat.NChatColor;
-import net.md_5.bungee.api.ChatColor;
+import com.nekozouneko.anni.game.MapManager;
+
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -38,6 +38,12 @@ public class ANNIAdminCommand implements CommandExecutor, TabCompleter {
                     }
                 } else if (args[1].equalsIgnoreCase("edit")) {
 
+                } else if (args[1].equalsIgnoreCase("list")) {
+                    listMap(sender, command, label, args);
+                }
+            } else if (args[0].equalsIgnoreCase("lobby")) {
+                if (args[1].equalsIgnoreCase("spawn")) {
+                    setSpawnOfLobby(sender, command, label, args);
                 }
             }
         }
@@ -50,7 +56,7 @@ public class ANNIAdminCommand implements CommandExecutor, TabCompleter {
         List<String> tab = new ArrayList<>();
 
         if (args.length == 1) {
-            final String[] arg = new String[] {"map"};
+            final String[] arg = new String[] {"kit", "lobby", "map"};
 
             for (String a : arg) {
                 if (a.toLowerCase().startsWith(args[0].toLowerCase())) {
@@ -58,11 +64,27 @@ public class ANNIAdminCommand implements CommandExecutor, TabCompleter {
                 }
             }
         } else if (args.length == 2) {
-            final String[] arg = new String[] {"add", "edit", "list", "remove"};
+            if (args[0].equalsIgnoreCase("map")) {
+                final String[] arg = new String[]{"add", "edit", "list", "reload", "remove"};
 
-            for (String a : arg) {
-                if (a.toLowerCase().startsWith(args[1].toLowerCase())) {
-                    tab.add(a);
+                for (String a : arg) {
+                    if (a.toLowerCase().startsWith(args[1].toLowerCase())) {
+                        tab.add(a);
+                    }
+                }
+            } else if (args[0].equalsIgnoreCase("lobby")) {
+                final String[] arg = new String[]{"spawn"};
+            } else if (args[0].equalsIgnoreCase("kit")) {
+
+            }
+        } else if (args.length == 4) {
+            if (args[0].equalsIgnoreCase("map") && args[1].equalsIgnoreCase("edit")) {
+                final String[] arg = new String[]{"display", "nexus", "rule"};
+
+                for (String a : arg) {
+                    if (a.toLowerCase().startsWith(args[3].toLowerCase())) {
+                        tab.add(a);
+                    }
                 }
             }
         }
@@ -99,5 +121,51 @@ public class ANNIAdminCommand implements CommandExecutor, TabCompleter {
             sender.sendMessage("IO Exception called");
         }
 
+    }
+
+    public static void listMap(CommandSender sender, Command cmd, String label, String[] args) {
+        MapManager mm = ANNIPlugin.getMM();
+
+        sender.sendMessage("Maps: ");
+        for (String k : mm.getMaps().keySet()) {
+            ANNIMap mp = mm.getMaps().get(k);
+            sender.sendMessage("- " + mp.getDisplay() + " (" + mp.getWorld() + ")");
+        }
+    }
+
+    public static void setSpawnOfLobby(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+
+            try {
+                ANNILobby annil = new ANNILobby(p.getLocation());
+                final Gson gson = new Gson();
+                ANNIPlugin ap = ANNIPlugin.getInstance();
+
+                BufferedWriter wr = new BufferedWriter(
+                        new OutputStreamWriter(
+                                new FileOutputStream(
+                                        new File(ap.getDataFolder(), "lobby.json")
+                                ),
+                                StandardCharsets.UTF_8
+                        )
+                );
+                gson.toJson(annil, ANNILobby.class, wr);
+
+                wr.flush();
+                wr.close();
+
+                sender.sendMessage("ロビーを以下の座標に設定しました:");
+                sender.sendMessage(
+                        "W: "
+                        + p.getLocation().getWorld().getName()
+                        + " X: " + p.getLocation().getX()
+                        + " Y: " + p.getLocation().getY()
+                        + " Z: " + p.getLocation().getZ()
+                        + " YW: " + p.getLocation().getYaw()
+                        + " P: " + p.getLocation().getPitch()
+                );
+            } catch (IOException ie) {}
+        }
     }
 }
