042cc75632cb47f76e0813f6995f20616599d60b
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-11-28 23:52:56 +0900

    Update: ゲーム管理システムの一部実装

--- spigot/src/main/java/com/nekozouneko/anni/game/GameManager.java
+++ spigot/src/main/java/com/nekozouneko/anni/game/GameManager.java
@@ -1,5 +1,6 @@
 package com.nekozouneko.anni.game;
 
+import com.nekozouneko.anni.ANNIPlugin;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
@@ -10,9 +11,25 @@ public class GameManager {
 
     private final Map<String, ANNIGame> games = new HashMap<>();
     private final Map<UUID, ANNIPlayer> players = new HashMap<>();
+    private final MapManager mm = ANNIPlugin.getMM();
 
     public GameManager() {
+        mm.getMaps().forEach((k, v) -> games.put(k, new ANNIGame(v)));
+    }
 
+    public boolean isJoined(Player p) {
+        return players.containsKey(p.getUniqueId());
+    }
+
+    public boolean join(String w, Player p) {
+        if (!isJoined((p))) {
+            if (mm.getMaps().containsKey(w)) {
+                ANNIGame g = games.get(w);
+                return true;
+            }
+        }
+
+        return false;
     }
 
 
