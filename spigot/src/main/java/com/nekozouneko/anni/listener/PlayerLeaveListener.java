328f587d5bda8ecd37aedfe948954193ad326805
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-11-28 23:54:08 +0900

    Update: 抜けたらスコアボードを削除するようにする

--- spigot/src/main/java/com/nekozouneko/anni/listener/PlayerLeaveListener.java
+++ spigot/src/main/java/com/nekozouneko/anni/listener/PlayerLeaveListener.java
@@ -1,17 +1,21 @@
 package com.nekozouneko.anni.listener;
 
 import com.nekozouneko.anni.ANNIPlugin;
+import fr.minuskube.netherboard.Netherboard;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerLeaveListener implements Listener {
 
+    private final Netherboard nb = ANNIPlugin.getNb();
+
     @EventHandler
     public void onEvent(PlayerQuitEvent e) {
         Player p = e.getPlayer();
 
         ANNIPlugin.getGM().leaveFromAllGames(p);
+        if (nb.getBoard(p) != null) nb.getBoard(p).delete();
     }
 
 }
