3e1dc446ed2ed00d648c6e6aab30ab0834d0aacb
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-11-28 23:53:38 +0900

    Update: 仮

--- spigot/src/main/java/com/nekozouneko/anni/game/ANNIPlayer.java
+++ spigot/src/main/java/com/nekozouneko/anni/game/ANNIPlayer.java
@@ -1,4 +1,16 @@
 package com.nekozouneko.anni.game;
 
+import org.bukkit.entity.Player;
+
 public class ANNIPlayer {
+
+    private final Player bukkitPlayer;
+
+    protected ANNIPlayer(Player p) {
+        this.bukkitPlayer = p;
+    }
+
+    public Player getBukkitPlayer() {
+        return bukkitPlayer;
+    }
 }
