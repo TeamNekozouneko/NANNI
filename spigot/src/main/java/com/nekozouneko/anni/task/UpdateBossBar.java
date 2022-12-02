71c18df33fe1f73248e05c08408a69d8e15ab1be
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-12-02 19:33:04 +0900

    Update: bossbarの仮組み作成

--- spigot/src/main/java/com/nekozouneko/anni/task/UpdateBossBar.java
+++ spigot/src/main/java/com/nekozouneko/anni/task/UpdateBossBar.java
@@ -2,6 +2,7 @@ package com.nekozouneko.anni.task;
 
 import com.nekozouneko.anni.game.ANNIGame;
 import com.nekozouneko.anni.game.ANNIStatus;
+import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BossBar;
 import org.bukkit.boss.KeyedBossBar;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -13,6 +14,8 @@ public class UpdateBossBar extends BukkitRunnable {
 
     private final BossBar bb;
     private final ANNIGame g;
+    private Map.Entry<ANNIStatus, Integer> timer;
+
     private static final Map<ANNIStatus, String> message = new HashMap<>(Map.of(
             ANNIStatus.CANT_START, "開始不可 (設定不足)",
             ANNIStatus.PHASE_ONE, "フェーズ 1",
@@ -25,6 +28,18 @@ public class UpdateBossBar extends BukkitRunnable {
             ANNIStatus.WAITING, "待機中",
             ANNIStatus.STOPPING, "ゲームを停止中 (運営の操作が必要です)"
     ));
+    private static final Map<ANNIStatus, Integer> phase_TIME = new HashMap<>(Map.of(
+            ANNIStatus.CANT_START, -1,
+            ANNIStatus.PHASE_ONE, 600,
+            ANNIStatus.PHASE_TWO, 600,
+            ANNIStatus.PHASE_THREE, 600,
+            ANNIStatus.PHASE_FOUR, 600,
+            ANNIStatus.PHASE_FIVE, 600,
+            ANNIStatus.PHASE_SIX, 600,
+            ANNIStatus.PHASE_SEVEN, 600,
+            ANNIStatus.WAITING, 60,
+            ANNIStatus.STOPPING, -1
+    ));
 
     public UpdateBossBar(ANNIGame g) {
         this.g = g;
@@ -33,7 +48,40 @@ public class UpdateBossBar extends BukkitRunnable {
 
     @Override
     public void run() {
+        if (g.getStatus() == ANNIStatus.CANT_START) return;
 
+        switch (g.getStatus()) {
+            case STOPPING:
+                stoppingCase(g.getStatus());
+                break;
+            case CANT_START:
+                cantStartCase(g.getStatus());
+                break;
+        }
+    }
+
+    private void cantStartCase(ANNIStatus stats) {
+        bb.setProgress(1.0);
+        bb.setColor(BarColor.RED);
+        bb.setTitle(message.get(stats));
+    }
+
+    private void stoppingCase(ANNIStatus stats) {
+        bb.setProgress(1.0);
+        bb.setColor(BarColor.RED);
+        bb.setTitle(message.get(stats));
+    }
+
+    private void waitingCase(ANNIStatus stats) {
+        final int min = g.getManager().getMinPlayers();
+        final int max = g.getManager().getMaxPlayers();
+        final int ps = g.getPlayers().size();
+
+        if (min <= ps && max >= ps) {
+
+        } else {
+
+        }
     }
 
 }
