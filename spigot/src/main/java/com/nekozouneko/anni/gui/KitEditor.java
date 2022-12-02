79a8c8a87ae006376e2b506f274bdd575676c7c5
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-12-02 23:27:20 +0900

    Add: KitEditorの仮組み

---
+++ spigot/src/main/java/com/nekozouneko/anni/gui/KitEditor.java
@@ -0,0 +1,20 @@
+package com.nekozouneko.anni.gui;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class KitEditor {
+
+    public void open(String k, Player p) {
+
+    }
+
+    public boolean isHandleable(InventoryClickEvent e) {
+        return false;
+    }
+
+    public void handle(InventoryClickEvent e) {
+
+    }
+
+}
