796a9cb0ad514f872353a4f4721be8f29b2e52be
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-12-02 23:25:21 +0900

    Update: サバイバルじゃないだけで跳ね返さないように

--- spigot/src/main/java/com/nekozouneko/anni/listener/BlockDestroyListener.java
+++ spigot/src/main/java/com/nekozouneko/anni/listener/BlockDestroyListener.java
@@ -4,6 +4,9 @@ import com.nekozouneko.anni.ANNIPlugin;
 import com.nekozouneko.anni.ANNIUtil;
 import com.nekozouneko.anni.Team;
 import com.nekozouneko.anni.event.NexusAttackEvent;
+import com.nekozouneko.anni.gui.MapEditor;
+import com.nekozouneko.anni.gui.location.AbstractGUILocationSelector;
+import com.nekozouneko.anni.gui.location.NexusLocation;
 import com.nekozouneko.anni.util.BlockDestroyUtil;
 import org.bukkit.*;
 import org.bukkit.event.EventHandler;
@@ -20,109 +23,117 @@ public class BlockDestroyListener implements Listener {
 
     @EventHandler
     public void onEvent(BlockBreakEvent e) {
-        if (e.getPlayer().getGameMode() != GameMode.SURVIVAL) return;
-
         World w = e.getBlock().getWorld();
         Location loc = e.getBlock().getLocation();
         Material bt = e.getBlock().getType();
 
-        switch (bt) {
-            case END_STONE:
-                e.setDropItems(false);
-                BlockDestroyUtil.nexusDestroyParticleSound(loc);
+        if (e.getPlayer().getGameMode() == GameMode.SURVIVAL) {
+            switch (bt) {
+                case END_STONE:
+                    e.setDropItems(false);
+                    BlockDestroyUtil.nexusDestroyParticleSound(loc);
 
-                Bukkit.getScheduler().runTaskLater(
-                        plugin, () -> loc.getBlock().setType(Material.END_STONE), 3
-                );
-                Bukkit.getServer().getPluginManager().callEvent(new NexusAttackEvent(e.getPlayer(), Team.RED));
-                break;
-            case MELON:
-                e.setDropItems(false);
-                e.getPlayer().getInventory().addItem(new ItemStack(Material.MELON_SLICE, r.nextInt(9) + 1));
+                    Bukkit.getScheduler().runTaskLater(
+                            plugin, () -> loc.getBlock().setType(Material.END_STONE), 3
+                    );
+                    Bukkit.getServer().getPluginManager().callEvent(new NexusAttackEvent(e.getPlayer(), Team.RED));
+                    break;
+                case MELON:
+                    e.setDropItems(false);
+                    e.getPlayer().getInventory().addItem(new ItemStack(Material.MELON_SLICE, r.nextInt(9) + 1));
 
-                Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.MELON), 100);
-                break;
-            case END_STONE_BRICKS:
-                e.setDropItems(false);
-                BlockDestroyUtil.finalNexusDestroyParticleSound(loc);
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.MELON), 100);
+                    break;
+                case END_STONE_BRICKS:
+                    e.setDropItems(false);
+                    BlockDestroyUtil.finalNexusDestroyParticleSound(loc);
 
-                Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.BEDROCK), 3);
-                break;
-            case COAL_ORE:
-                e.setDropItems(false);
-                e.setExpToDrop(0);
-                if (ANNIUtil.isMineableOre(bt, e.getPlayer().getInventory().getItemInMainHand().getType())) {
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.BEDROCK), 3);
+                    break;
+                case COAL_ORE:
+                    e.setDropItems(false);
+                    e.setExpToDrop(0);
+                    if (ANNIUtil.isMineableOre(bt, e.getPlayer().getInventory().getItemInMainHand().getType())) {
+                        e.getPlayer().getInventory().addItem(
+                                new ItemStack(
+                                        ANNIUtil.getOreMinedResult(bt),
+                                        ANNIUtil.getFortuneOreDropItemAmounts(e.getPlayer())
+                                )
+                        );
+                    }
+
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.BEDROCK), 3);
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.COAL_ORE), 140);
+                    break;
+                case IRON_ORE:
+                case REDSTONE_ORE:
+                case LAPIS_ORE:
+                    e.setDropItems(false);
+                    e.setExpToDrop(0);
+                    if (ANNIUtil.isMineableOre(bt, e.getPlayer().getInventory().getItemInMainHand().getType())) {
+                        e.getPlayer().getInventory().addItem(
+                                new ItemStack(
+                                        ANNIUtil.getOreMinedResult(bt),
+                                        ANNIUtil.getFortuneOreDropItemAmounts(e.getPlayer())
+                                )
+                        );
+                    }
+
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.BEDROCK), 3);
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(bt), 200);
+                    break;
+                case GOLD_ORE:
+                    e.setDropItems(false);
+                    e.setExpToDrop(0);
+                    if (ANNIUtil.isMineableOre(bt, e.getPlayer().getInventory().getItemInMainHand().getType())) {
+                        e.getPlayer().getInventory().addItem(
+                                new ItemStack(
+                                        ANNIUtil.getOreMinedResult(bt), ANNIUtil.getFortuneOreDropItemAmounts(e.getPlayer())
+                                )
+                        );
+                    }
+
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.BEDROCK), 3);
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(bt), 300);
+                    break;
+                case DIAMOND_ORE:
+                case EMERALD_ORE:
+                    e.setDropItems(false);
+                    e.setExpToDrop(0);
+                    if (ANNIUtil.isMineableOre(bt, e.getPlayer().getInventory().getItemInMainHand().getType())) {
+                        e.getPlayer().getInventory().addItem(
+                                new ItemStack(
+                                        ANNIUtil.getOreMinedResult(bt),
+                                        ANNIUtil.getFortuneOreDropItemAmounts(e.getPlayer())
+                                )
+                        );
+                    }
+
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.BEDROCK), 3);
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(bt), 600);
+                    break;
+                case GRAVEL:
+                    e.setDropItems(false);
+                    e.setExpToDrop(0);
                     e.getPlayer().getInventory().addItem(
                             new ItemStack(
-                                    ANNIUtil.getOreMinedResult(bt),
+                                    Material.FLINT,
                                     ANNIUtil.getFortuneOreDropItemAmounts(e.getPlayer())
                             )
                     );
-                }
 
-                Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.BEDROCK), 3);
-                Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.COAL_ORE), 140);
-                break;
-            case IRON_ORE:
-            case REDSTONE_ORE:
-            case LAPIS_ORE:
-                e.setDropItems(false);
-                e.setExpToDrop(0);
-                if (ANNIUtil.isMineableOre(bt, e.getPlayer().getInventory().getItemInMainHand().getType())) {
-                    e.getPlayer().getInventory().addItem(
-                            new ItemStack(
-                                    ANNIUtil.getOreMinedResult(bt),
-                                    ANNIUtil.getFortuneOreDropItemAmounts(e.getPlayer())
-                            )
-                    );
-                }
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.BEDROCK), 3);
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(bt), 100);
+                default:
+                    break;
+            }
+        }
 
-                Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.BEDROCK), 3);
-                Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(bt), 200);
-                break;
-            case GOLD_ORE:
-                e.setDropItems(false);
-                e.setExpToDrop(0);
-                if (ANNIUtil.isMineableOre(bt, e.getPlayer().getInventory().getItemInMainHand().getType())) {
-                    e.getPlayer().getInventory().addItem(
-                            new ItemStack(
-                                    ANNIUtil.getOreMinedResult(bt), ANNIUtil.getFortuneOreDropItemAmounts(e.getPlayer())
-                            )
-                    );
-                }
-
-                Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.BEDROCK), 3);
-                Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(bt), 300);
-                break;
-            case DIAMOND_ORE:
-            case EMERALD_ORE:
-                e.setDropItems(false);
-                e.setExpToDrop(0);
-                if (ANNIUtil.isMineableOre(bt, e.getPlayer().getInventory().getItemInMainHand().getType())) {
-                    e.getPlayer().getInventory().addItem(
-                            new ItemStack(
-                                    ANNIUtil.getOreMinedResult(bt),
-                                    ANNIUtil.getFortuneOreDropItemAmounts(e.getPlayer())
-                            )
-                    );
-                }
-
-                Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.BEDROCK), 3);
-                Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(bt), 600);
-                break;
-            case GRAVEL:
-                e.setDropItems(false);
-                e.setExpToDrop(0);
-                e.getPlayer().getInventory().addItem(
-                        new ItemStack(
-                                Material.FLINT,
-                                ANNIUtil.getFortuneOreDropItemAmounts(e.getPlayer())
-                        )
-                );
-
-                Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(Material.BEDROCK), 3);
-                Bukkit.getScheduler().runTaskLater(plugin, () -> loc.getBlock().setType(bt), 100);
-            default: break;
+        AbstractGUILocationSelector g = MapEditor.glc.get(e.getPlayer().getUniqueId());
+        if (g != null) {
+            if (g instanceof NexusLocation) {
+                g.edit(e.getBlock().getLocation());
+            }
         }
     }
 
