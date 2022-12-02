1eb2b45b8e7a75da449c8f7a9f16eb3931ed046b
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-12-02 23:25:40 +0900

    Update: add waiting mode

--- spigot/src/main/java/com/nekozouneko/anni/task/UpdateBoard.java
+++ spigot/src/main/java/com/nekozouneko/anni/task/UpdateBoard.java
@@ -4,10 +4,12 @@ import com.nekozouneko.anni.ANNIPlugin;
 import com.nekozouneko.anni.Team;
 import com.nekozouneko.anni.file.ANNIMap;
 import com.nekozouneko.anni.game.ANNIGame;
+import com.nekozouneko.anni.game.ANNIStatus;
 import com.nekozouneko.anni.game.GameManager;
 import fr.minuskube.netherboard.Netherboard;
 import fr.minuskube.netherboard.bukkit.BPlayerBoard;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -46,30 +48,48 @@ public class UpdateBoard extends BukkitRunnable {
                 ANNIMap m = annig.getMap();
                 String mn = (m != null ? m.getDisplay() : "?");
 
-                if (gm.getRuleType() == 2) {
+                if (gm.getGame().getStatus() == ANNIStatus.WAITING
+                || annig.getStatus() == ANNIStatus.STOPPING || ANNIStatus.CANT_START == annig.getStatus())
+                {
+                    Team t = annig.getPlayerJoinedTeam(p);
+                    String tn = t != null ? annig.getScoreBoardTeam(t).getDisplayName() + "" : "無所属";
+
                     b.setAll(
-                            "§8" + annig.getId16() + " " + sdf.format(d),
+                            "§8" + annig.getId16(),
                             "   ",
-                            "マップ: §c" + mn,
+                            "あなたは" + tn + "です。",
                             "  ",
-                            "§c赤: §7" + annig.getNexusHealth(Team.RED),
-                            "§9青: §7" + annig.getNexusHealth(Team.BLUE),
+                            "現在" + annig.getPlayers().size() + "人が参加しています",
                             " ",
                             "§9§nnekozouneko.net"
                     );
                 } else {
-                    b.setAll(
-                            "§8" + annig.getId16() + " " + sdf.format(d),
-                            "   ",
-                            "マップ: §c" + mn,
-                            "  ",
-                            "§c赤: §7" + annig.getNexusHealth(Team.RED),
-                            "§9青: §7" + annig.getNexusHealth(Team.BLUE),
-                            "§e黄: §7" + annig.getNexusHealth(Team.YELLOW),
-                            "§a緑: §7" + annig.getNexusHealth(Team.GREEN),
-                            " ",
-                            "§9§nnekozouneko.net"
-                    );
+                    if (gm.getRuleType() == 2) {
+                        b.setAll(
+                                "§8" + annig.getId16(),
+                                "   ",
+                                "マップ: §c" + mn,
+                                "フェーズ: §c" + annig.getStatus().getPhaseId(),
+                                "  ",
+                                "§c赤: §7" + annig.getNexusHealth(Team.RED),
+                                "§9青: §7" + annig.getNexusHealth(Team.BLUE),
+                                " ",
+                                "§9§nnekozouneko.net"
+                        );
+                    } else {
+                        b.setAll(
+                                "§8" + annig.getId16() + " " + sdf.format(d),
+                                "   ",
+                                "マップ: §c" + mn,
+                                "  ",
+                                "§c赤: §7" + annig.getNexusHealth(Team.RED),
+                                "§9青: §7" + annig.getNexusHealth(Team.BLUE),
+                                "§e黄: §7" + annig.getNexusHealth(Team.YELLOW),
+                                "§a緑: §7" + annig.getNexusHealth(Team.GREEN),
+                                " ",
+                                "§9§nnekozouneko.net"
+                        );
+                    }
                 }
             } else if (p.getWorld() == ANNIPlugin.getLobby().getLocation().getBukkitWorld()) {
                 if (b == null) b = nb.createBoard(p, "\u00A7cA\u00A79N\u00A7eN\u00A7aI");
