6aa274a9f99d96d37e7c387672dfe5e2123e9c7a
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-12-04 23:01:14 +0900

    Update: add Phase cases

--- spigot/src/main/java/com/nekozouneko/anni/task/UpdateBossBar.java
+++ spigot/src/main/java/com/nekozouneko/anni/task/UpdateBossBar.java
@@ -1,10 +1,13 @@
 package com.nekozouneko.anni.task;
 
+import com.nekozouneko.anni.ANNIUtil;
+import com.nekozouneko.anni.Team;
 import com.nekozouneko.anni.game.ANNIGame;
 import com.nekozouneko.anni.game.ANNIStatus;
 import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarFlag;
 import org.bukkit.boss.BossBar;
-import org.bukkit.boss.KeyedBossBar;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.AbstractMap;
@@ -47,11 +50,19 @@ public class UpdateBossBar extends BukkitRunnable {
     public UpdateBossBar(ANNIGame g) {
         this.g = g;
         this.bb = g.getBossBar();
+
+        bb.setColor(BarColor.BLUE);
+        bb.removeFlag(BarFlag.CREATE_FOG);
+        bb.removeFlag(BarFlag.PLAY_BOSS_MUSIC);
+        bb.removeFlag(BarFlag.DARKEN_SKY);
     }
 
     @Override
     public void run() {
         if (g.getStatus() == ANNIStatus.CANT_START) return;
+
+        bb.removeAll();
+        for (Player p : g.getPlayers()) bb.addPlayer(p);
 
         switch (g.getStatus()) {
             case STOPPING:
@@ -60,6 +71,25 @@ public class UpdateBossBar extends BukkitRunnable {
             case CANT_START:
                 cantStartCase(g.getStatus());
                 break;
+            case WAITING:
+                waitingCase(g.getStatus());
+                break;
+            case PHASE_ONE:
+                phase1Case(g.getStatus());
+                break;
+            case PHASE_TWO:
+                phase2Case(g.getStatus());
+                break;
+            case PHASE_THREE:
+                phase3Case(g.getStatus());
+        }
+    }
+
+    @Override
+    public void cancel() {
+        if (!isCancelled()) {
+            bb.removeAll();
+            super.cancel();
         }
     }
 
@@ -77,14 +107,85 @@ public class UpdateBossBar extends BukkitRunnable {
 
     private void waitingCase(ANNIStatus stats) {
         final int min = g.getManager().getMinPlayers();
-        final int max = g.getManager().getMaxPlayers();
-        final int ps = g.getPlayers().size();
+        final int ps;
+        int psr = g.getPlayers(Team.RED).size();
+        int psb = g.getPlayers(Team.BLUE).size();
+        if (g.getManager().getRuleType() == 4) {
+            int psg = g.getPlayers(Team.GREEN).size();
+            int psy = g.getPlayers(Team.YELLOW).size();
 
-        if (min <= ps && max >= ps) {
+            ps = psr+psb+psg+psy;
+        } else ps = psr+psb;
 
+        if (min <= ps) {
+            if (g.getTimer() <= -1) {
+                g.setTimer(phase_TIME.get(stats));
+            }
+
+            if (g.getTimer() == 0) {
+                g.changeStatus(ANNIStatus.PHASE_ONE);
+                bb.setProgress(ANNIUtil.bossBarProgress(phase_TIME.get(stats), g.getTimer()));
+                bb.setTitle(message.get(stats) + " - 開始まであと " + ANNIUtil.toTimerFormat(g.getTimer()));
+                g.setTimer(phase_TIME.get(ANNIStatus.PHASE_ONE));
+            } else {
+                bb.setProgress(ANNIUtil.bossBarProgress(phase_TIME.get(stats), g.getTimer()));
+                bb.setTitle(message.get(stats) + " - 開始まであと " + ANNIUtil.toTimerFormat(g.getTimer()));
+                g.takeTimer();
+            }
         } else {
+            if (g.getTimer() >= 0) {
+                g.broadcast("誰かが退出したため開始がキャンセルされました。");
+                g.setTimer(-1);
+            }
 
+            bb.setProgress(ANNIUtil.bossBarProgress(min, ps));
+            bb.setTitle(message.get(stats) + " - あと" + (min - ps) + "人必要です。");
         }
+    }
+
+    private void phase1Case(final ANNIStatus stats) {
+        if (g.getTimer() <= 0) {
+            bb.setTitle(message.get(stats) + " - " + ANNIUtil.toTimerFormat(g.getTimer()));
+            bb.setProgress(ANNIUtil.bossBarProgress(phase_TIME.get(stats), g.getTimer()));
+
+            g.setTimer(phase_TIME.get(ANNIStatus.PHASE_TWO));
+            g.changeStatus(ANNIStatus.PHASE_TWO);
+            return;
+        }
+
+        bb.setTitle(message.get(stats) + " - " + ANNIUtil.toTimerFormat(g.getTimer()));
+        bb.setProgress(ANNIUtil.bossBarProgress(phase_TIME.get(stats), g.getTimer()));
+        g.takeTimer();
+    }
+
+    private void phase2Case(final ANNIStatus stats) {
+        if (g.getTimer() <= 0) {
+            bb.setTitle(message.get(stats) + " - " + ANNIUtil.toTimerFormat(g.getTimer()));
+            bb.setProgress(ANNIUtil.bossBarProgress(phase_TIME.get(stats), g.getTimer()));
+
+            g.setTimer(phase_TIME.get(ANNIStatus.PHASE_THREE));
+            g.changeStatus(ANNIStatus.PHASE_THREE);
+            return;
+        }
+
+        bb.setTitle(message.get(stats) + " - " + ANNIUtil.toTimerFormat(g.getTimer()));
+        bb.setProgress(ANNIUtil.bossBarProgress(phase_TIME.get(stats), g.getTimer()));
+        g.takeTimer();
+    }
+
+    private void phase3Case(final ANNIStatus stats) {
+        if (g.getTimer() <= 0) {
+            bb.setTitle(message.get(stats) + " - " + ANNIUtil.toTimerFormat(g.getTimer()));
+            bb.setProgress(ANNIUtil.bossBarProgress(phase_TIME.get(stats), g.getTimer()));
+
+            g.setTimer(phase_TIME.get(ANNIStatus.PHASE_FOUR));
+            g.changeStatus(ANNIStatus.PHASE_FOUR);
+            return;
+        }
+
+        bb.setTitle(message.get(stats) + " - " + ANNIUtil.toTimerFormat(g.getTimer()));
+        bb.setProgress(ANNIUtil.bossBarProgress(phase_TIME.get(stats), g.getTimer()));
+        g.takeTimer();
     }
 
 }
