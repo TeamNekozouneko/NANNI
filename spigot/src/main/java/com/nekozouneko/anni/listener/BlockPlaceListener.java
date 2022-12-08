7065dc3dca00d78ec807e64a87f5ce324549a397
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-12-08 20:02:07 +0900

    Update: Cannot be placed within a 10 block radius of the Nexus
    * ネクサスの半径10m以内は設置不可能に

--- spigot/src/main/java/com/nekozouneko/anni/listener/BlockPlaceListener.java
+++ spigot/src/main/java/com/nekozouneko/anni/listener/BlockPlaceListener.java
@@ -1,9 +1,15 @@
 package com.nekozouneko.anni.listener;
 
+import com.nekozouneko.anni.ANNIPlugin;
 import com.nekozouneko.anni.gui.MapEditor;
 import com.nekozouneko.anni.gui.location.AbstractGUILocationSelector;
 import com.nekozouneko.anni.gui.location.NexusLocation;
 import com.nekozouneko.anni.gui.location.TeamSpawnPointLocation;
+import com.nekozouneko.anni.util.SimpleLocation;
+import com.nekozouneko.nutilsxlib.chat.NChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -12,6 +18,26 @@ public class BlockPlaceListener implements Listener {
 
     @EventHandler
     public void onEvent(BlockPlaceEvent e) {
+
+        if (ANNIPlugin.getGM().getGame().isJoined(e.getPlayer())) {
+            for (SimpleLocation l : ANNIPlugin.getGM().getGame().getMap().getNexusList().values()) {
+                Location fl = l.toLocation(ANNIPlugin.getGM().getGame().getCopiedMap());
+
+                if (
+                        ((fl.getX()-10d) <= e.getBlock().getLocation().getX()
+                        && (fl.getX()+10d) >= e.getBlock().getLocation().getX()) &&
+                        ((fl.getY()-10d) <= e.getBlock().getLocation().getY()
+                        && (fl.getY()+10d) >= e.getBlock().getLocation().getY()) &&
+                        ((fl.getZ()-10d) <= e.getBlock().getLocation().getZ()
+                        && (fl.getZ()+10d) >= e.getBlock().getLocation().getZ())
+                        && e.getPlayer().getGameMode() == GameMode.SURVIVAL
+                ) {
+                    e.getPlayer().sendMessage(NChatColor.RED + "ネクサス付近は設置はできません。");
+                    e.setCancelled(true);
+                }
+            }
+        }
+
         AbstractGUILocationSelector g = MapEditor.glc.get(e.getPlayer().getUniqueId());
         if (g != null) {
             if (g instanceof TeamSpawnPointLocation) {
