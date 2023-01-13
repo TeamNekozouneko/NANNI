b122cbff7fa39b9ccde2fd9e97cdf3cc8918f13f
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-01-13 15:04:27 +0900

    Update: 特殊なチェーン装備は落下耐性をつけるように

--- spigot/src/main/java/com/nekozouneko/anni/listener/PlayerDamageListener.java
+++ spigot/src/main/java/com/nekozouneko/anni/listener/PlayerDamageListener.java
@@ -1,25 +1,73 @@
 package com.nekozouneko.anni.listener;
 
+import com.nekozouneko.anni.ANNIPlugin;
+import com.nekozouneko.anni.util.BooleanDataType;
 import com.nekozouneko.nutilsxlib.chat.NChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerItemBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.potion.PotionEffectType;
 
 public class PlayerDamageListener implements Listener {
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
+    public void onDamage(EntityDamageEvent e) {
+        if (e.getEntity() instanceof Player) {
+            Player p = ((Player) e.getEntity());
+
+            final NamespacedKey nofall = new NamespacedKey(ANNIPlugin.getInstance(), "specialbuffarmor");
+
+            if (p.getInventory().getBoots() != null) {
+                ItemStack boot = p.getInventory().getBoots();
+
+                if (boot.getItemMeta() != null) {
+                    PersistentDataContainer pdc = boot.getItemMeta().getPersistentDataContainer();
+                    if (pdc.has(nofall, new BooleanDataType())) {
+                        if (pdc.get(nofall, new BooleanDataType()) == true) {
+                            if (e.getCause() == EntityDamageEvent.DamageCause.FALL && !e.isCancelled()) {
+                                Damageable d = (Damageable) p.getInventory().getBoots().getItemMeta();
+                                if (e.getDamage() < 20) {
+                                    d.setDamage(d.getDamage() + 9);
+                                } else d.setDamage(d.getDamage() + ((int)e.getDamage()/2));
+
+                                if (d.getDamage() > boot.getType().getMaxDurability()) {
+                                    d.setDamage(boot.getType().getMaxDurability());
+                                    /*p.getInventory().setBoots(null);
+                                    p.playSound(p.getLocation(), Sound.ENTITY_ITEM_BREAK, 1, 1);
+                                    p.spawnParticle(Particle.ITEM_CRACK, p.getLocation(), 10);
+                                    Bukkit.getServer().getPluginManager().callEvent(new PlayerItemBreakEvent(p, boot));*/
+                                } else e.setCancelled(true);
+                                p.getInventory().getBoots().setItemMeta(d);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
     public void onEvent(EntityDamageByEntityEvent e) {
         if (e.getEntity() instanceof Player && e.getDamager() instanceof Player) {
             Player p = (Player) e.getEntity();
 
-            if (p.hasPotionEffect(PotionEffectType.INVISIBILITY)) {
-                String d = e.getDamager().getCustomName();
-                d = d == null ? e.getDamager().getName() : d;
-                p.removePotionEffect(PotionEffectType.INVISIBILITY);
-                p.sendMessage(NChatColor.RED + d + "の攻撃により透明化が解除されました");
-                p.getLocation().getWorld().playSound(p.getLocation(), Sound.ENTITY_GENERIC_EXTINGUISH_FIRE, 1, 0);
+            if (!e.isCancelled()) {
+                if (p.hasPotionEffect(PotionEffectType.INVISIBILITY)) {
+                    String d = e.getDamager().getCustomName();
+                    d = d == null ? e.getDamager().getName() : d;
+                    p.removePotionEffect(PotionEffectType.INVISIBILITY);
+                    p.sendMessage(NChatColor.RED + d + "の攻撃により透明化が解除されました");
+                    p.getLocation().getWorld().playSound(p.getLocation(), Sound.ENTITY_GENERIC_EXTINGUISH_FIRE, 1, 0);
+                }
             }
         }
     }
